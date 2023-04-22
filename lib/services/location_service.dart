5d9ceb8f951d4f1c89af25fd9372c12b4a19c5f6
OengOeiyy ♥ <89408540+oengoeiyy@users.noreply.github.com>
2023-04-22 21:39:15 +0700

    return key

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class LocationService {
-  final String key = 'YourKeyKa';
+  final String key = 'AIzaSyD6SmOYJNbMJFyD2cgkArJMHJtS9zfaTPE';
 
   Future<String> getPlaceID(String input) async {
     final String url =
